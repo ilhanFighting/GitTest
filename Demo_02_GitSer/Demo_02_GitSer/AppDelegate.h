2478efadaf7f325b9b19c4576a41ef3bd6c93f20
tarena3007 <tarena3007@tarena3001deiMac-2.local>
2015-10-08 11:55:58 +0800

    123

---
+++ Demo_02_GitSer/Demo_02_GitSer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_02_GitSer
+//
+//  Created by tarena3007 on 15/10/8.
+//  Copyright (c) 2015年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_02_GitSer/Demo_02_GitSer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_02_GitSer
+//
+//  Created by tarena3007 on 15/10/8.
+//  Copyright (c) 2015年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
